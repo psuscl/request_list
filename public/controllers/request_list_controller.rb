6a65dc1221c5b56987fcd65876acba01b12da621
jambun <jambun@gmail.com>
2018-04-20 19:00:15 +1000

    Don't blow up when viewing an empty list

--- public/controllers/request_list_controller.rb
+++ public/controllers/request_list_controller.rb
@@ -1,5 +1,10 @@
 class RequestListController <  ApplicationController
   def index
-    @results = archivesspace.search_records(JSON.parse(cookies['as_pui_request_list_list_contents']), {})
+    list = JSON.parse(cookies['as_pui_request_list_list_contents'])
+    if list.empty?
+      @results = false
+    else
+      @results = archivesspace.search_records(list, {})
+    end
   end
 end
