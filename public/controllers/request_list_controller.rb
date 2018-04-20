bd1cfe4284a02d5be8ee83a42c052b35a579ae0f
jambun <jambun@gmail.com>
2018-04-20 18:43:50 +1000

    Prettier request list with remove buttons

--- public/controllers/request_list_controller.rb
+++ public/controllers/request_list_controller.rb
@@ -1,8 +1,5 @@
 class RequestListController <  ApplicationController
   def index
-
-    @list = JSON.parse(cookies['as_pui_request_list_list_contents']);
-
-    puts "CCCCCCCC #{@list.inspect}"
+    @results = archivesspace.search_records(JSON.parse(cookies['as_pui_request_list_list_contents']), {})
   end
 end
