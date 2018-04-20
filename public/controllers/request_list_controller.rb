2256da9d652e03703b01e609220d20287ae5bf8b
jambun <jambun@gmail.com>
2018-04-21 00:54:10 +1000

    First cut at sending the requests

--- public/controllers/request_list_controller.rb
+++ public/controllers/request_list_controller.rb
@@ -5,6 +5,7 @@ class RequestListController <  ApplicationController
       @results = false
     else
       @results = archivesspace.search_records(list, {})
+      @system = AppConfig[:request_list][:systems][:test_aeon]
     end
   end
 end
