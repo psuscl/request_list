cc8e01e278eefd8b99892b8dd622cd698a4e2e09
jambun <jambun@gmail.com>
2018-04-20 12:23:42 +1000

    Add and remove logic for button

--- public/assets/request_list.js
+++ public/assets/request_list.js
@@ -11,25 +11,90 @@
 	return $.cookie.apply(this, args);
     };
 
-    RequestList.prototype.showListCount = function() {
-	var items = JSON.parse(this.cookie("list_contents")).length;
+    RequestList.prototype.getList = function() {
+	return JSON.parse(this.cookie("list_contents"));
+    };
+
+    RequestList.prototype.setList = function(list) {
+	this.cookie("list_contents", JSON.stringify(list));
+    };
+
+    RequestList.prototype.isInList = function(uri) {
+	var list = this.getList();
+	return !($.inArray(uri, list) == -1);
+    };
+
+    RequestList.prototype.updateButtonState = function() {
+	var button = $("#request_list_action_button");
+	if (button.length > 0) {
+	    if (this.isInList(button.data("uri"))) {
+		var i = button.find("i." + button.data("add-icon"));
+		if (i) {
+		    i.removeClass(button.data("add-icon"));
+		    i.addClass(button.data("remove-icon"));
+		    button.attr("title", button.data("remove-label"));
+		    button.html(button.html().replace(button.data("add-label"), button.data("remove-label")));
+		}
+	    } else {
+		var i = button.find("i." + button.data("remove-icon"));
+		if (i) {
+		    i.removeClass(button.data("remove-icon"));
+		    i.addClass(button.data("add-icon"));
+		    button.attr("title", button.data("add-label"));
+		    button.html(button.html().replace(button.data("remove-label"), button.data("add-label")));
+		}
+	    }
+	}
+    };
+
+    RequestList.prototype.showListCount = function(flash) {
+	var items = this.getList().length;
 	var a = $("a[href='/plugin/request_list']").first();
 	a.html(a.html().replace(/ \(\d+\)/, " (" + items + ")"));
-	a.parent().fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+	if (flash) {
+	    a.parent().fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+	}
     };
 
     RequestList.prototype.addToList = function(uri) {
-	var list = JSON.parse(this.cookie("list_contents"));
-	list.push(uri);
-	this.cookie("list_contents", JSON.stringify(list));
-	this.showListCount();
+	var list = this.getList();
+	if (this.isInList(uri)) {
+	    return false;
+	} else {
+	    list.push(uri);
+	    this.setList(list);
+	    this.showListCount(true);
+	    return true;
+	}
+    };
+
+    RequestList.prototype.removeFromList = function(uri) {
+	var list = this.getList();
+	if (this.isInList(uri)) {
+	    list.splice($.inArray(uri, list), 1);
+	    this.setList(list);
+	    this.showListCount(true);
+	    return true;
+	} else {
+	    return false;
+	}
+    };
+
+    RequestList.prototype.toggleListItem = function(uri) {
+	if (this.isInList(uri)) {
+	    this.removeFromList(uri);
+	} else {
+	    this.addToList(uri);
+	}
+	this.updateButtonState();
     };
 
     RequestList.prototype.setUpList = function() {
 	if (!this.cookie("list_contents")) {
-	    this.cookie("list_contents", JSON.stringify([]));
+	    this.setList([]);
 	}
 	    
+	this.updateButtonState();
 	this.showListCount();
     };
 
--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -9,9 +9,6 @@ Plugins::extend_aspace_routes(File.join(File.dirname(__FILE__), "routes.rb"))
 
 Plugins::add_menu_item('/plugin/request_list', 'plugin.request_list.menu_label')
 
-Plugins::add_record_page_action_js(cfg.fetch(:record_types, ['archival_object', 'accession']),
-                                   'plugin.request_list.add_to_list_button_label',
-                                   cfg.fetch(:icon, 'fa-indent'), 
-                                   "request_list.addToList($(this).data('uri'));",
-                                   cfg.fetch(:button_position, nil))
-
+Plugins::add_record_page_action_erb(cfg.fetch(:record_types, ['archival_object', 'accession']),
+                                    'request_list/action_button',
+                                    cfg.fetch(:button_position, nil))
