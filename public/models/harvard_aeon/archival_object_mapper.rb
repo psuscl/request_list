5b3a58d497eebd0a6496ab38a540f24b6b808af2
jambun <jambun@gmail.com>
2018-05-07 13:12:53 +1000

    Moved mappers into module for profile

---
+++ public/models/harvard_aeon/archival_object_mapper.rb
@@ -0,0 +1,20 @@
+module HarvardAeon
+  class ArchivalObjectMapper < RequestListItemMapper
+
+    include HarvardCommon
+    include ManipulateNode
+
+    RequestList.register_item_mapper(self, :harvard_aeon, ArchivalObject)
+
+    def map(item)
+      num = get_request_number
+      {
+        'Request' => num, 
+        "ItemTitle_#{num}" => strip_mixed_content(item['title']),
+        "Location_#{num}" => repo_field_for(item, 'Location'),
+        "Site_#{num}" => repo_field_for(item, 'Site'),
+      }
+    end
+
+  end
+end
---
+++ public/models/harvard_aeon/list_mapper.rb
@@ -0,0 +1,17 @@
+module HarvardAeon
+  class ListMapper < RequestListMapper
+
+    RequestList.register_list_mapper(self, :harvard_aeon)
+
+    def map
+      {
+        'SystemId' => 'ArchivesSpace',
+        'ReturnLinkURL' => (AppConfig[:public_proxy_url] || AppConfig[:public_url]) + '/plugin/request_list',
+        'ReturnLinkSystemName' => @opts[:return_link_label],
+        'RequestType' => 'Loan',
+        'UserReview' => 'No',
+      }
+    end
+
+  end
+end
