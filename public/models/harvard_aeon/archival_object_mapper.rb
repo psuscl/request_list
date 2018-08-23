0f6ad9ce8991ab91a63507e0d80eb80009b68402
jambun <jambun@gmail.com>
2018-08-23 15:30:16 +1000

    Map collection restrictions to ItemInfo2 for AOs

--- public/models/harvard_aeon/archival_object_mapper.rb
+++ public/models/harvard_aeon/archival_object_mapper.rb
@@ -22,8 +22,8 @@ module HarvardAeon
         'SubLocation'    => mapped.ext(:physical_location).name,
         'ItemInfo3'      => mapped.extent.multi.map {|e| [e.name, e.ext(:container_summary), e.ext(:physical_details)].select {|e| !e.blank?}.join(", ")}.join('; '),
         'CallNumber'     => mapped.collection.id,
-        'ItemInfo2'      => mapped.record.ext(:access_restrictions),
-        'ItemPublisher'  => mapped.collection.ext(:access_restrictions),
+#        'ItemInfo2'      => mapped.record.ext(:access_restrictions),
+        'ItemInfo2'      => mapped.collection.ext(:access_restrictions),
         'ItemIssue'      => mapped.record.id,
       }
 
