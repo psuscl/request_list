da423a603a03e56e4b335dbdb6b57567775a0d20
jambun <jambun@gmail.com>
2018-09-17 12:42:24 +1000

    Improved item display from partial

--- public/mailers/request_list_mailer.rb
+++ public/mailers/request_list_mailer.rb
@@ -1,10 +1,10 @@
 class RequestListMailer < ApplicationMailer
   def email(user_email, mapper)
     # the plugin dir is not in the view path in this context, so render the template manually
-    view = ActionView::Base.new(ActionController::Base.view_paths, {})
-    email_body = view.render(file: File.join(File.dirname(__FILE__), '..', 'views', 'request_list_mailer', 'email'),
-                             locals: {mapper: mapper},
-                             layout: 'layouts/mailer')
+    view_path = File.join(File.dirname(__FILE__), '..', 'views')
+    email_body = render(file: File.join(view_path, 'request_list_mailer', 'email'),
+                        locals: {mapper: mapper, view_path: view_path},
+                        layout: 'layouts/mailer')
 
     mail(
          from: I18n.t('plugin.request_list.email.from'),
