4107a233484653e210ab8e497547ad135316ad1b
Kevin Clair <kmc35@psu.edu>
2022-10-18 15:51:49 -0400

    let pennstate_pui_customizations handle adding this to the header

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -5,8 +5,6 @@ cfg = AppConfig[:request_list]
 
 Plugins::extend_aspace_routes(File.join(File.dirname(__FILE__), "routes.rb"))
 
-Plugins::add_menu_item('/plugin/request_list/pennstate', 'plugin.request_list.menu_label')
-
 Plugins::add_record_page_action_erb(cfg.fetch(:record_types, ['archival_object', 'accession']),
                                     'request_list/action_button',
                                     cfg.fetch(:button_position, nil))
